6cc817474b947ca6f148890ac51b89ef27059fa2
Albert Bori <github@albertbori.com>
2022-08-19 15:12:03 -0400

    Auto Publish Docs (#6)
    * added github action for publishing docs
    
    * removed docs folder
    
    * make docs script executable

--- generate-docs.sh
+++ generate-docs.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+# This script is invoked by /.github/workflows/pages.yml
+# It is kept separate so that it can be tested locally/manually
+# The ./docs path is needed by /.github/workflows/pages.yml for deployment
 swift package --allow-writing-to-directory ./docs \
     generate-documentation --target VSM \
     --output-path ./docs \
