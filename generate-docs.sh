0d339bb3baab50448b17aec933d6efa0613f96c9
Albert Bori <github@albertbori.com>
2022-08-19 13:21:02 -0400

    fixed comment script. bug

--- generate-docs.sh
+++ generate-docs.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
 # This script is invoked by /.github/workflows/pages.yml
 # It is kept separate so that it can be tested locally/manually
+# The ./docs path is needed by /.github/workflows/pages.yml for deployment
 swift package --allow-writing-to-directory ./docs \
     generate-documentation --target VSM \
-    --output-path ./docs \ # This path is coordinated with /.github/workflows/pages.yml
+    --output-path ./docs \
     --hosting-base-path vsm-ios \
     --transform-for-static-hosting \
     --disable-indexing
