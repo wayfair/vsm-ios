2e9db01f2e112a01dc12b134892286d535db8e49
Ethan van Heerden <evanheerden@wayfair.com>
2024-12-03 13:17:33 -0500

    Bump TestableCombinePublishers

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
-        .package(url: "https://github.com/albertbori/TestableCombinePublishers.git", from: "1.2.1")
+        .package(url: "https://github.com/albertbori/TestableCombinePublishers.git", from: "2.0.1")
     ],
     targets: [
         .target(
